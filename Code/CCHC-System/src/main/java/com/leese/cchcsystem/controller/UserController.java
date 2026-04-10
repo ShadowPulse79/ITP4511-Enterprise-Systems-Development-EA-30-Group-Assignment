f0d7f6b0dc94ce7a3560e524ee29be61baba4f6d
leese852 <88673132+leese852@users.noreply.github.com>
2026-04-10 15:42:27 +0800

    write:  user module

--- Code/CCHC-System/src/main/java/com/leese/cchcsystem/controller/UserController.java
+++ Code/CCHC-System/src/main/java/com/leese/cchcsystem/controller/UserController.java
@@ -1,4 +1,74 @@
 package com.leese.cchcsystem.controller;
 
-public class UserController {
+import com.leese.cchcsystem.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet({
+        "/user/login",
+        "/user/register",
+        "/user/logout",
+        "/user/profile",
+        "/user/updateInfo",
+        "/user/changePassword"
+})
+public class UserController extends HttpServlet {
+    private UserService userService = new UserService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+        String path = request.getServletPath();
+        if ("/user/login".equals(path)) {
+            showLoginPage(request, response);
+        } else if ("/user/register".equals(path)) {
+            showRegisterPage(request, response);
+        } else if ("/user/logout".equals(path)) {
+            logout(request, response);
+        } else if ("/user/profile".equals(path)) {
+            showProfilePage(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String path = request.getServletPath();
+
+        if ("/user/login".equals(path)) {
+            login(request, response);
+        } else if ("/user/register".equals(path)) {
+            register(request, response);
+        } else if ("/user/updateProfile".equals(path)) {
+            updateProfile(request, response);
+        } else if ("/user/changePassword".equals(path)) {
+            changePassword(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+
+    }
+
+    private void showLoginPage(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+    }
+
+    private void showRegisterPage(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/register.jsp").forward(request, response);
+    }
+
+    private void showProfilePage(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession
+    }
 }
